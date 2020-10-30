4f5450de2f001b3141172deba7314001128d02b6
it63070003 <73647989+it63070003@users.noreply.github.com>
2020-10-30 10:54:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-<?php
-header("Location: index.html");
-exit();
-?>
+<html>
+    <head>
+        <title>OofBlox</title>
+    </head>
+    <body>
+        <h1>Welcome To OofBlox</h1>
+        <img src="oofhead.jpg">
+        <p>Oof Looks like you need OOF Partner, Welp you come to right place</p>
+        <a href="https://roblox.com">Enter Roblox</a>
+        <br>
+        <h2>Jokes!</h2>h2>
+        <p>มีอยู่วันหนึ่ง ณ โรงเรียนแห่งหนึ่งมีจรเข้ นก ตัวตุ่น และไก่ ไก่ถามจรเข้ว่าพ่อนายเป็นนักอะไร จรเข้ตอบ เป็นนักว่ายน้ำ จากนั้น จรเข้ถาม นก ว่าพ่อนายเป็นนักอะไร นก ตอบกลับ เป็นนักบินแล้ว นก ถามตัวตุ่นว่าพ่อนายเป็นนักอะไร ตัวตุ่นตอบกลับว่า นักขุด แล้วทุกตัวก็ถามไก่ว่า พ่อนายเป็นนักอะไร ไก่ตอบกลับมาว่า นักเก็ต</p>
+        <br>
+        <h2>Owner</h2>
+        <img src="oof.png">
+        <p>Hello!, i'm oof man the Owner of OofBlox. Here you can find your OOF Partner</p>
+        <br>
+        <p>Btw my name is,กรรษฎา มงคลจาตุรงค์ and my ชื่อเล่น is โมเด็ม</p>
+        <br>
+        <h2>Partner Lists</h2>
+        <p>Here's your oof partner name list!</p>
+        <ul>
+            <li>OOFman69</li>
+            <li>PiggyInwza777</li>
+            <li>KutchaWant2Cry</li>
+            <li>MaoNaja_Gaming</li>
+            <li>EksdeeBoy88</li>
+        </ul>
+    </body>
+</html>

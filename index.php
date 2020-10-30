13cc57fb20427f8d3aa600e5f1bf5aeeda223311
it63070003 <73647989+it63070003@users.noreply.github.com>
2020-10-30 10:19:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php
-echo "<h1>Hi</hi>"
+<? php
+echo '<html>';
+echo '<head>';
+echo '<title>OofBlox</title>';
+echo '</head>';
+echo '<body>';
+echo '<h1>Welcome To OofBlox</h1>';
+echo '<img src="oofhead.jpg">';
+echo '<p>Oof Looks like you need OOF Partner, Welp you come to right place</p>';
+echo '<a href="https://roblox.com">Enter Roblox</a>';
+echo '<br>';
+echo '<h2>Owner</h2>';
+echo '<img src="oof.png">';
+echo '<p>Hello!, i'm oof man the Owner of OofBlox. Here you can find your OOF Partner</p>';
+echo '<br>';
+echo '<p>Btw my name is,นายกรรษฎา มงคลจาตุรงค์ and my ชื่อเล่น is โมเด็ม</p>';
+echo '<br>';
+echo '<h2>Partner Lists</h2>';
+echo '<p>Here's your oof partner name list!</p>';
+echo '<ul>';
+echo '<li>OOFman69</li>';
+echo '<li>PiggyInwza777</li>';
+echo '<li>KutchaWant2Cry</li>';
+echo '<li>MaoNaja_Gaming</li>';
+echo '<li>EksdeeBoy88</li>';
+echo '</ul>';
+echo '</body>';
+echo '</html>';
+echo '';
 ?>
